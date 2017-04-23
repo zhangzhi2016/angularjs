b15e1d0a14245196f5aedc6ccd14b8091c9542c8
张志 <2767462109@qq.com>
2017-04-23 18:35:00 +0800

    Delete route.js

--- route.js
+++
@@ -1,9 +0,0 @@
-﻿// 添加路由
-
-app.config(['$routeProvider',function( $routeProvider ){
-  $routeProvider
-  .when('/',{templateUrl: 'view/dynamic.html'})
-  .when('/message',{templateUrl: 'view/message.html'})
-  .when('/introduction',{templateUrl: 'view/introduction.html'})
-  .otherwise({redirectTo:'view/404.html'});
-}]);
